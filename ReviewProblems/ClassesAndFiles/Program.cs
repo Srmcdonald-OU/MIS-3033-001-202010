9330765819fdeb6df04632be7483212d95fcddc3
srmcdonald-OU <stephen.r.mcdonald-1@ou.edu>
2020-09-01 18:02:42 -0500

    Completed the Student UML and did some cool datetime thingamabobs

--- ReviewProblems/ClassesAndFiles/Program.cs
+++ ReviewProblems/ClassesAndFiles/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ClassesAndFiles
 {
@@ -6,7 +7,40 @@ namespace ClassesAndFiles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Student> students = new List<Student>();
+                
+            Student stud1 = new Student();
+            Console.WriteLine($"{stud1.name} has an id of {stud1.id}");
+            stud1.name = "Micah";
+            stud1.id = 2;
+
+            Student stud2 = new Student("Adam", 1);
+            students.Add(stud1);
+            students.Add(stud2);
+
+            foreach (var stud in students)
+            {
+                
+                Console.WriteLine(stud);
+            }
+            DateTime current = DateTime.Now;
+            DateTime past = Convert.ToDateTime("1/1/2020");
+
+            var results = current - past;
+
+            Console.WriteLine($"It has been {results.TotalDays} days since the New Year, which was on a {past.DayOfWeek}");
+
+            TimeSpan difference = current - stud1.enrollDate;
+
+            Console.WriteLine($"It has been {difference.TotalMilliseconds} ms since {stud1.name} has enrolled");
+
+            Console.WriteLine($"There are currently {Student.totalstudents} enrolled");
+
+            //Console.WriteLine($"{stud1.name} has an id of {stud1.id}");
+            //Console.WriteLine($"{stud2.name} has an id of {stud2.id}");
+
+            //Dictionary can be used to avoid having to iterate thru a List
+
         }
     }
 }
---
+++ ReviewProblems/ClassesAndFiles/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndFiles  //Class
+{
+    class Student
+    {
+        public string name { get; set; }  // - is private.  + is pubilc
+        public int id { get;  set; }  //Use the get; and set; format to save time by default
+        public static int totalstudents;  //Static variable is shared across all instances
+        public DateTime enrollDate { get; set; }
+
+        public Student(string name, int id)  : this() //Constructor.  this() calls Student() then it goes into Student(string,int)
+        {
+            this.name = name;     //Explicitly says this. is the instance where Name is using.
+            this.id = id;
+            //this.enrollDate = DateTime.Now;  //This. not needed here since it can't be confused
+            totalstudents++;
+        }
+
+        public Student()  //Default constructor
+        {
+            name = string.Empty;    //Default empty string
+            id = -1;                //Default -1
+            enrollDate = DateTime.Now;
+            totalstudents++;
+        }
+
+
+        public string getName()
+        {
+            return name; //Publically returns the Name of the called instance of the class
+        }  //Getter
+
+        public void setName(string name)
+        {
+            this.name = name;  //sets the name of the current student
+        }
+
+        public override string ToString()
+        {
+            //return "Programming is sooo much fun";
+            return $"{name} has an id of {id}";  //changes the output
+        }
+    }
+}
